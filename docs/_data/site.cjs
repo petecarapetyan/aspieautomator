162e4283af843fa7dd9a66bc9e98464cf08cf2dd
petecarapetyan <pete@datafundamentals.com>
2021-05-02 19:16:50 -0500

    WIP

--- docs/_data/site.cjs
+++ docs/_data/site.cjs
@@ -3,20 +3,22 @@ module.exports = function () {
     dir: 'ltr',
     lang: 'en',
     name: 'Aspie Automator',
-    description: 'not-alphabet is a customizeable theme, for the Rocket SSG',
+    description: "One person's experiences on autism spectrum",
     socialLinks: [
       {
         name: 'GitHub',
-        url: 'https://github.com/petecarapetyan/fins-rocket-themes/tree/main/not-alphabet/src/docs',
+        url: 'https://github.com/petecarapetyan/aspieautomator',
       },
     ],
-    gitSiteUrl: 'https://github.com/petecarapetyan/fins-rocket-themes',
+    gitSiteUrl: 'https://github.com/petecarapetyan/aspieautomator',
     gitBranch: 'main',
-    helpUrl: 'https://github.com/petecarapetyan/fins-rocket-themes/issues',
-    logoAlt: 'Rocket Logo',
+    helpUrl: 'https://github.com/petecarapetyan/aspieautomator/issues',
+    logoAlt: 'Aspie Automator',
     iconColorMaskIcon: '#3f93ce',
     iconColorMsapplicationTileColor: '#1d3557',
     iconColorThemeColor: '#1d3557',
     socialMediaImage: '/_assets/social-media-image.jpg',
+    analyticsUA: 'UA-52870008-3',
+    analyticsGA4: 'G-7JNV14GM0W'
   };
 };
